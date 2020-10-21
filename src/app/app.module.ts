a0fd1339136ba57392d297ef60eebea17adc0fea
najem-anass <najem20anass@gmail.com>
2020-10-21 18:42:56 +0100

    guard pour l'autentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,12 @@ import { auth } from 'firebase';
 import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const appRoutes : Routes = [
   {path:""    , component : AppareilViewComponent},
-  {path:"appareils" , component : AppareilViewComponent},
-  {path:"appareils/:id",component:SingleAppareilComponent},
+  {path:"appareils" , canActivate: [AuthGuardService], component : AppareilViewComponent},
+  {path:"appareils/:id", canActivate: [AuthGuardService] , component:SingleAppareilComponent},
   {path:"auth"  ,   component : AuthComponent},
   {path:"not-found",component:FourOhFourComponent},
   {path:'**' , redirectTo:"/not-found"}
